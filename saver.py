d4aa6d14954a880a833b12ccac9bbf73cc54eb21
Kalio4 <114572679+Kalio4@users.noreply.github.com>
2022-12-05 22:32:53 +0300

    Add files via upload

---
+++ saver.py
@@ -0,0 +1,10 @@
+from pytube import YouTube
+
+link = input("Link: ")
+
+yt = YouTube(link)
+ys = yt.streams.get_highest_resolution()
+
+print("indiriliyor...")
+ys.download()
+print("indirildi! zixen tarafindan basit bir kodla yazilmistir.")
